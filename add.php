d45064a2e1421d43c7a8d9175bbe062bb0d9f79b
Paxton Houston <Paxton.Houston11@gmail.com>
2026-01-28 17:17:00 -0600

    fixed add.php

--- add.php
+++ add.php
@@ -1,18 +1,19 @@
 <?php
-$shapasswd = "please fill in sha256 hashed password :]"
-$blog_temp = "<!DOCTYPE html>\n<html>\n<head>\n<title>" . $_POST["title"] . "</title>\n</head>\n<body>\n<a href='../../../index.html'>back</a>\n<hr>\n<h2>" . $_POST["title"] . "</h2>\n" . str_replace("\n", "<br>", $_POST["text"]) . "\n<br>\n<i>posted on " . date("m/d/Y h:i A") . " UTC</i>\n<hr>\n</body>\n</html>";
-
-if (hash("sha256", $_POST["password"]) == $shapasswd) {
 date_default_timezone_set('UTC');
+$shapasswd = "please fill in sha256 hashed password :]";
+$blog_temp = "<!DOCTYPE html>\n<html>\n<head>\n<title>" . $_POST["title"] . "</title>\n</head>\n<body>\n<a href='../../../index.html'>back</a>\n<hr>\n<h2>" . $_POST["title"] . "</h2>\n" . str_replace("\n", "<br>", $_POST["text"]) . "\n<br>\n<i>posted on " . date("m/d/Y h:i A") . " UTC</i>\n<hr>\n</body>\n</html>";
+$stupid = date("Y") . "/" . date("m") . "/" . date("d");
+
+//if (hash("sha256", $_POST["password"]) == $shapasswd) {
 
 //make the year's directory and add it to main page
-if (!is_dir(date(Y))) {
-    mkdir(date(Y));
+if (!is_dir(date("Y"))) {
+    mkdir(date("Y"));
     $f = fopen("index.html", "r+");
 
     $oldstr = file_get_contents("index.html");
-    $str_to_insert = "<details>\n<summary>" . date(Y) . "</summary>\n<!--posts" . date(Y) . "-->\n</details>\n";
-    $specificLine = "years";
+    $str_to_insert = "<details>\n<summary>" . date("Y") . "</summary>\n<!--posts" . date("Y") . "-->\n</details>\n";
+    $specificLine = "ys";
 
     while (($buffer = fgets($f)) !== false) {
         if (strpos($buffer, $specificLine) !== false) {
@@ -24,29 +25,30 @@ if (!is_dir(date(Y))) {
     }
     fclose($f);
 }
-
 //same thing but don't add to main page and also month
-if (!is_dir(date(Y) . "/" . date(m))) {
-    mkdir(date(Y) . "/" . date(m));
+if (!is_dir(date("Y") . "/" . date("m"))) {
+    mkdir(date("Y") . "/" . date("m"));
 }
 
 //blah blah
-if (!is_dir(date(Y) . "/" . date(m) . "/" . date(d))) {
-    mkdir(date(Y) . "/" . date(m) . "/" . date(d));
+if (!is_dir(date("Y") . "/" . date("m") . "/" . date("d"))) {
+    mkdir(date("Y") . "/" . date("m") . "/" . date("d"));
 }
 
 //open the file
-$file = fopen(date(Y) . "/" . date(m) . "/" date(d) . "/index.html", "w+");
+$file = fopen("" . $stupid . "/index.html", "w+");
 echo "opened the file";
 
 //actually add the blog post
-file_put_contents(date(Y) . "/" . date(m) . "/" date(d) . "/index.html", $blog_temp);
+file_put_contents("" . $stupid . "/index.html", $blog_temp);
+fclose($file);
 
 //add the thing to the main page's years collumn
 $sf = fopen("index.html", "r+");
+
 $oldstr2 = file_get_contents("index.html");
-$str_to_insert2 = "<a href='" . date(Y) . "/" . date(m) . "/" date(d) . "/index.html" . "'>" . $_POST["title"] . "</a>\n<br>\n";
-$specificLine2 = "posts" . date(Y);
+$str_to_insert2 = "<a href='" . $stupid . "/index.html" . "'>" . $_POST["title"] . "</a>\n<br>\n";
+$specificLine2 = "posts" . date("Y");
 
 while (($buffer2 = fgets($sf)) !== false) {
     if (strpos($buffer2, $specificLine2) !== false) {
@@ -56,12 +58,12 @@ while (($buffer2 = fgets($sf)) !== false) {
         break;
     }
 }
-fclose($f2);
+fclose($sf);
 
 //do the same thing again
 $ssf = fopen("index.html", "r+");
 $oldstr23 = file_get_contents("index.html");
-$str_to_insert23 = "<a href='" . date(Y) . "/" . date(m) . "/" date(d) . "/index.html" . "'><h2>" . $_POST["title"] . "</h2><a>\n<br>\n<i><p>posted on" . date(m/d/Y) . "</p></i>\n<br>\n";
+$str_to_insert23 = "<a href='" . $stupid . "/index.html" . "'><h2>" . $_POST["title"] . "</h2><a>\n<br>\n<i><p>posted on" . date("m/d/Y") . "</p></i>\n<br>\n";
 $specificLine23 = "titles";
 
 while (($buffer23 = fgets($ssf)) !== false) {
@@ -72,12 +74,11 @@ while (($buffer23 = fgets($ssf)) !== false) {
         break;
     }
 }
-fclose($f23);
+fclose($ssf);
 
-//close file and redirect
-fclose($file);
-header("Location: " . date(Y) . "/" . date(m) . "/" date(d) . "/index.html");
-} else {
-   echo "wrong password";
-}
+//redirect
+header("Location: " . $stupid . "/index.html");
+//} else {
+//   echo "wrong password";
+//}
 ?>
